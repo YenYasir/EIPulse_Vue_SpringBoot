b2b1974af410405724c0789acafd44f4f1c6b625
u06 m4d63 <u06m4d93@gmail.com>
2023-11-06 18:31:17 +0800

    rm git include stupid one

--- EIPulse/teamproject/src/main/java/com/eipulse/teamproject/repository/employeerepository/TitleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eipulse.teamproject.repository.employeerepository;
-
-
-import com.eipulse.teamproject.entity.employee.Title;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TitleRepository extends JpaRepository<Title, Integer> {
-}
