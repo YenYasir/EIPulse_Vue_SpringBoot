1ec037d4b8008cc74977fef8772da35e4a07adac
wke1998 <ygkhoph@gmail.com>
2023-11-09 18:59:15 +0800

    1109下午更新

---
+++ eip-vue-vite/src/views/employee/Emergency.vue
@@ -0,0 +1,106 @@
+<script setup >
+import { ref, computed, reactive } from 'vue';
+import Swal from 'sweetalert2'
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+
+const router = useRouter();
+const data = reactive([]);
+
+const addHandler = async () => {
+    // 這個 API 剩下缺少 empId 的輸入
+    try {
+        const response = await axios.post('http://localhost:8090/eipulse/emergency/add', data)
+        Swal.fire({
+            title: '增加成功',
+            timer: 1000,
+            timerProgressBar: true,
+            icon: 'success'
+        });
+        console.log(data);
+    } catch (e) {
+        Swal.fire({
+            title: '資料有誤請確認',
+            icon: 'error'
+        });
+        console.error(e);
+    }
+};
+
+</script>
+
+
+<template>
+    <form @submit.prevent="addHandler">
+        <div style=" margin-top: 100px; ">
+            <h2>新增緊急聯絡人資料</h2>
+            <table class="form-table" style="width: 410px; ">
+                <!-- 家屬姓名 -->
+                <tr>
+                    <th><label for="emergencyName" class="form-label">姓名</label></th>
+                    <td><input type="text" class="form-control" id="emergencyName" required></td>
+                </tr>
+
+                <!-- 關係 -->
+                <tr>
+                    <th><label for="relation" class="form-label">關係</label></th>
+                    <td><input type="text" class="form-control" id="relation" required></td>
+                </tr>
+                <!-- 電話 -->
+                <tr>
+                    <th><label for="phone" class="form-label">連絡電話</label></th>
+                    <td><input type="tel" class="form-control" id="phone" required></td>
+                </tr>
+
+            </table>
+            <button class="btn btn-primary" type="submit" style="width: 400px;">
+                新增
+            </button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+h2 {
+    text-align: center;
+}
+
+/* 為表格添加圓角 */
+.form-table {
+    width: 50%;
+    margin: 20px auto 10px auto;
+    border-collapse: separate;
+    /* 使用separate而非collapse以使border-radius生效 */
+    border-spacing: 0;
+    /* 移除單元格間的間距 */
+    border-radius: 10px;
+    /* 圓角設置 */
+    overflow: hidden;
+    /* 隱藏內部超出部分 */
+}
+
+/* 為單元格添加邊線 */
+
+
+
+
+.form-table th {
+    text-align: center;
+    background-color: #a3c4e8;
+}
+
+.form-control {
+    width: 98%;
+    padding: 10px;
+    margin: 5px;
+    box-sizing: border-box;
+    border-color: #0161c7;
+}
+
+.btn {
+    width: 50%;
+    display: block;
+    margin: 2px auto;
+}
+</style>
---
+++ eip-vue-vite/src/views/form/FormList.vue
@@ -0,0 +1,199 @@
+<script setup>
+import axios from "axios";
+import { computed, reactive, ref } from "vue";
+import ShowFormObj from "../../components/form/ShowFormObj.vue";
+import Swal from "sweetalert2";
+import FormPage from "../../components/form/FormPage.vue";
+import { empStore } from "../../stores/employee.js";
+const emp = empStore();
+const URL = import.meta.env.VITE_API_JAVAURL;
+let toDay = new Date();
+let formattedDate = toDay.toISOString().split('T')[0]; // 這將給你 "2023-10-20" 這樣的格式
+const startDate = ref(false);
+
+const page = reactive({
+  totalPages: 0,
+  currentPage: 0,
+});
+const forms = ref([]);
+
+// 下面是進階查詢
+const types = reactive(["全部", "請假", "加班", "離職"]);
+const leave = reactive([
+  "全部",
+  "半年特休",
+  "一年特休",
+  "半薪病假",
+  "生理假",
+  "事假",
+  "婚假",
+  "喪假",
+  "產假",
+]);
+const overtime = reactive([
+  "全部",
+  "平日",
+  "休息日",
+  "國定假日或特別休假",
+  "例假",
+]);
+const stusts = reactive([
+  "全部",
+  "審核中",
+  "已批准",
+  "已拒絕",
+  "待處理",
+  "過期",
+  "撤回",
+]);
+const formSelect = reactive({
+  formType: 0,
+  type: 0,
+  empId: emp.empId,
+  stustId: 0,
+  startTime: '',
+  endTime: '',
+});
+let dataSelect = reactive({
+  formType: 0,
+  type: 0,
+  empId: "",
+  stustId: 0,
+  startTime: "",
+  endTime: "",
+});
+const savevalue = () => {
+  dataSelect = JSON.parse(JSON.stringify(formSelect));
+  selectForm(1);
+};
+const pg = ref();
+const selectForm = async (nowpage) => {
+  if (nowpage === null) {
+    nowpage = 1;
+  }
+  let tempFormSelect = { ...dataSelect };
+  if (tempFormSelect.formType == 0 || tempFormSelect.formType == 3) {
+    tempFormSelect.type = null;
+  } else if (tempFormSelect.type == 0) {
+    tempFormSelect.type = null;
+  }
+  if (tempFormSelect.stustId == 0) {
+    tempFormSelect.stustId = "";
+  }
+  if (tempFormSelect.startTime != "") {
+    tempFormSelect.startTime = tempFormSelect.startTime + "T00:00:00";
+  }
+  if (tempFormSelect.endTime != "") {
+    tempFormSelect.endTime = tempFormSelect.endTime + "T00:00:00";
+  }
+  const URLAPI = `${URL}form/selectForms?pageNumber=${nowpage - 1}&pageSize=5`;
+  const response = await axios.post(URLAPI, tempFormSelect);
+  console.log(tempFormSelect)
+  forms.value = response.data.content;
+  page.totalPages = parseInt(response.data.totalPages);
+  page.currentPage = parseInt(response.data.pageable.pageNumber) + 1;
+  pg.value = nowpage;
+};
+
+const revokeForm = async (empId, formId) => {
+  Swal.fire({
+    title: "確定要執行此操作嗎？",
+    text: "這個操作將無法撤銷。",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "確定",
+    cancelButtonText: "取消",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const URLAPI = `${URL}form/revoke?empId=${empId}&formId=${formId}`;
+      const response = axios.put(URLAPI);
+      Swal.fire("操作已執行", "", "success");
+      selectForm(pg.value);
+    } else {
+      Swal.fire("操作已取消", "", "info");
+    }
+  });
+};
+
+const formatStartDate = (dateString) => {
+  if (dateString == null) {
+    return "尚未批改";
+  }
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  const date = new Date(dateString);
+  return date.toLocaleDateString("zh-TW", options);
+};
+</script>
+<template>
+  <div class="card text-center div1">
+    <h2 style="margin: 20px 0">查詢自己送出的表單</h2>
+    <select v-model="formSelect.formType">
+      <option v-for="(type, index) in types" :value="index">{{ type }}</option>
+    </select>
+    <select v-model="formSelect.type" v-if="(formSelect.formType != 0) & (formSelect.formType != 3)">
+      <option v-for="(type, index) in leave" :value="index" v-if="formSelect.formType == 1">
+        {{ type }}
+      </option>
+      <option v-for="(type, index) in overtime" :value="index" v-if="formSelect.formType == 2">
+        {{ type }}
+      </option>
+    </select>
+    <select v-model="formSelect.stustId">
+      <option v-for="(stust, index) in stusts" :value="index">
+        {{ stust }}
+      </option>
+    </select>
+    <label>
+      <input type="checkbox" v-model="startDate" value="option1" />
+      日期搜尋
+    </label>
+    <template v-if="startDate == true">
+      開始日期
+      <input type="date" v-model="formSelect.startTime" />
+      結束日期
+      <input type="date" v-model="formSelect.endTime" />
+    </template>
+    <button @click="savevalue">進階搜尋</button>
+    <div class="card-header"></div>
+    <div class="card-body">
+      <table class="table">
+        <tr>
+          <th data-sortable="true">表單ID</th>
+          <th>表單類型</th>
+          <th>創建時間</th>
+          <th>更新日期</th>
+          <th>審核人</th>
+          <th>審核狀態</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(form, index) in forms" :key="form.formId">
+          <td>{{ form.formId }}</td>
+          <td>{{ form.typeName }}</td>
+          <td>{{ formatStartDate(form.startDate) }}</td>
+          <td>{{ formatStartDate(form.endDate) }}</td>
+          <td>{{ form.auditor }}</td>
+          <td>{{ form.statusName }}</td>
+          <td>
+            <ShowFormObj :datas="form" :formType="form.typeId" />
+            <button @click="revokeForm(form.empId, form.formId)" v-if="form.statusId == 1">
+              撤回
+            </button>
+          </td>
+        </tr>
+      </table>
+      <FormPage :total-pages="page.totalPages" :current-page="page.currentPage" @page-change="selectForm" />
+    </div>
+  </div>
+</template>
+
+<style>
+.currentPage {
+  background-color: red;
+}
+</style>
