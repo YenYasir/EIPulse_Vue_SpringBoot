a5b5a7ee145cbdf9fab66a6b55d8682e5b4e64fa
wke1998 <ygkhoph@gmail.com>
2023-11-13 22:52:51 +0800

    1113新增公佈欄功能

--- EIPulse/teamproject/src/main/java/com/eipulse/teamproject/repository/employeerepository/EmergencyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eipulse.teamproject.repository.employeerepository;
-
-import com.eipulse.teamproject.entity.employee.Emergency;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmergencyRepository extends JpaRepository<Emergency, Integer> {
-}
